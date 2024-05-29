a6686e1a5b76b084042d100b6939eeaf5a52c69a
Hauser Fabian <120960@fhwn.ac.at>
2024-05-29 11:55:48 +0200

    deleted unecessary stuff

--- Mission_Planning_Tool/test/test.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime,timedelta
-
-scenario_start_timestamp_object = datetime.strptime('06-May-2024 20:15:04', '%d-%b-%Y %H:%M:%S')
-scenario_start_timestamp_grafana_format = scenario_start_timestamp_object.strftime("%d-%m-%Y %H:%M:%S")
-scenario_start_timestamp_grafana_format_object = datetime.strptime(scenario_start_timestamp_grafana_format, "%d-%m-%Y %H:%M:%S")
